05ff2a167a06c2fbf55a809ccda1e9b0a77bcfac
loopdelux <nihilisticschizoid@gmail.com>
2022-07-10 19:24:23 -0400

    updated dice.js and im using git-cli now sam

--- commands/dice.js
+++ commands/dice.js
@@ -1,3 +1,4 @@
 module.exports.onmessage = function(message, client) {
-    message.channel.send("You rolled a  :dice:" + Math.floor(Math.random() * 6 + 1))
+    const nums = ["one", "two", "three", "four", "five", "six"]
+    message.channel.send(":game_die: :" + nums[Math.floor(Math.random() * 6)] + ":")
 }
