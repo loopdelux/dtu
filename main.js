1594b3f05dda527972fcdc71a503d5db908042a3
raxracks <multisniperism@gmail.com>
2022-07-10 12:08:28 -0700

    switch commands to array and get rid of deprecated event

--- main.js
+++ main.js
@@ -1,48 +1,41 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
-const config = require("./config.json")
+const config = require("./config.json");
 
 const client = new Discord.Client({
-    // https://discord.com/developers/docs/topics/gateway#list-of-intents
-    intents: [
+  // https://discord.com/developers/docs/topics/gateway#list-of-intents
+  intents: [
     Discord.Intents.FLAGS.GUILDS,
     Discord.Intents.FLAGS.GUILD_MESSAGES,
     Discord.Intents.FLAGS.GUILD_MEMBERS,
-    ]   
-})
+  ],
+});
 
 // Command Handler ---------------------
-const fs = require('fs')
-let commands = {}
+const fs = require("fs");
+const commands = [];
 
-client.on('ready', () => fs.readdirSync('./commands').forEach(command => commands[command.split(".js")[0]] = command))
+client.on("ready", () =>
+  fs
+    .readdirSync("./commands")
+    .forEach((command) => commands.push(command.split(".js")[0]))
+);
 
-client.on('message', async message => {
-    let command = message.content.split(' ')[0]
+client.on("messageCreate", async (message) => {
+  if (!message.content.startsWith(config.prefix)) return;
+  let command = message.content.split(" ")[0].slice(1);
 
-    if(commands[command]) {
-        require(`./commands/${commands[command]}`).onmessage(message, client)
-    }
-})
+  if (commands.includes(command)) {
+    require(`./commands/${command}.js`).onmessage(message, client);
+  }
+});
 //--------------------------------------
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setActivity(`you.`, {
-        type: "WATCHING",
-      })
-})
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`you.`, {
+    type: "WATCHING",
+  });
+});
 
-// Just testing out discord.js
-client.on("message", (msg) => {
-
-    /* In case I ever re-add my message handler
-    if(msg.author.bot) {
-        return
-    }
-    */
-
-    console.log(msg)
-})
-
-client.login(config.token)
+client.login(config.token);
